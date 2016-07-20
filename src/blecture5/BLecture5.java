c4391b16a0ae2c33c97ce18b8d7f989fed1ad715
ZhouYibo <ZhouYibo@10.230.197.200>
2016-07-20 14:19:08 +0900

    new commit

--- src/blecture5/BLecture5.java
+++ src/blecture5/BLecture5.java
@@ -12,12 +12,13 @@ import javax.swing.JTextField;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.io.IOException;
 
 public class BLecture5 {
     private JTextField tf = null; 
     private JTextArea ta = null;
-    
     
 public BLecture5(){
     
@@ -30,45 +31,35 @@ public BLecture5(){
     ta = new JTextArea("本文用テキストエリア");
 
     JButton btn = new JButton("Load");
+    JButton btn1 = new JButton("Save");
     
     jf.setLayout(new BorderLayout());
     jf.add(addressBar, BorderLayout.NORTH);
     jf.add(new JScrollPane(ta), BorderLayout.CENTER);
-    //jf.add(btn, BorderLayout.SOUTH);
+    jf.add(btn1, BorderLayout.SOUTH);
     
     addressBar.setLayout(new BorderLayout());
     addressBar.add(btn, BorderLayout.EAST);
     addressBar.add(tf, BorderLayout.CENTER);
 
-    
-    
-    
     btn.addActionListener(new ActionListener() {
         public void actionPerformed(ActionEvent a) {
             ta.append(tf.getText()+"\n");
-            ta.append("aaa");
             String fname = tf.getText();
-            //Lecture4の二個目のファイル参照
-            //new FileInput(fname);
             
         FileReader fr;
 
             try {
-
             // 入力用のファイルリーダを生成
             fr = new FileReader(fname);
             } catch (FileNotFoundException e) {
             System.out.println(fname + "がオープンできません。");
             return;
             }
-
-            // 行単位で読み込むバッファ付きリーダを生成
-
             BufferedReader br = new BufferedReader(fr);
             String line;
             try {
             while ((line = br.readLine()) != null) {
-                System.out.println(line);
                 ta.append(line+"\n");
             }
 
@@ -78,6 +69,24 @@ public BLecture5(){
             }
         }
     });
+    
+    btn1.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent a){
+        String fname = tf.getText();
+          
+        try {
+            PrintWriter pw = new PrintWriter(new FileWriter(fname));
+            String str = ta.getText();
+            String[] strs = str.split("¥n");
+                for (int i = 0 ; i < strs.length ; i++){
+                    pw.println(strs[i]);
+                }
+            pw.close();
+            } catch (IOException e) {
+            e.printStackTrace(); }         
+        }
+    });
+    
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jf.setSize(320, 240);
         jf.setVisible(true);
